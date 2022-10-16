9f4f5520b67515f1ab6018d0450f9d4732801434
the80hz <dnl.vlgv@mail.ru>
2022-10-16 18:53:55 +0400

    Добавил везде исключения

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+class MyException : public std::exception {
+public:
+    explicit MyException(const char* msg) : msg_(msg) {}
+    const char* what() const noexcept override { return msg_; }
+private:
+    const char* msg_;
+};
+
+
 class bin_pic{
 private:
     bool **matrix;
@@ -33,60 +42,120 @@ public:
     }
     // set the value of a pixel
     void set(int x, int y, bool value){
-        matrix[x][y] = value;
+        try{
+            if(x >= maxX || y >= maxY){
+                throw MyException("Index out of bounds");
+            }
+            matrix[x][y] = value;
+        }
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
+        }
     }
     // get the value of a pixel
     bool get(int x, int y){
-        return matrix[x][y];
+        try{
+            if(x >= maxX || y >= maxY){
+                throw MyException("Index out of bounds");
+            }
+            return matrix[x][y];
+        }
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
+        }
+        return false;
     }
     // output the picture
     void print(){
-        for(int i = 0; i < maxX; i++){
-            for(int j = 0; j < maxY; j++){
-                std::cout << matrix[i][j];
+        try{
+            for(int i = 0; i < maxX; i++){
+                for(int j = 0; j < maxY; j++){
+                    if(matrix[i][j]){
+                        std::cout << "1";
+                    }
+                    else{
+                        std::cout << "0";
+                    }
+                }
+                std::cout << std::endl;
             }
-            std::cout << std::endl;
+        }
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
         }
     }
     // invert the picture
     bin_pic operator!(){
-        bin_pic pic(maxX, maxY);
-        for(int i = 0; i < maxX; i++){
-            for(int j = 0; j < maxY; j++){
-                pic.set(i, j, !matrix[i][j]);
+        try{
+            bin_pic pic(maxX, maxY);
+            for (int i = 0; i < maxX; i++) {
+                for (int j = 0; j < maxY; j++) {
+                    pic.set(i, j, !matrix[i][j]);
+                }
             }
+            return pic;
         }
-        return pic;
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
+        }
+        return {0, 0};
     }
     // add two pictures
     bin_pic operator+(const bin_pic &pic){
-        bin_pic new_pic(maxX, maxY);
-        for(int i = 0; i < maxX; i++){
-            for(int j = 0; j < maxY; j++){
-                new_pic.set(i, j, matrix[i][j] || pic.matrix[i][j]);
+        try{
+            if(pic.maxX != maxX || pic.maxY != maxY){
+                throw MyException("Dimensions do not match");
             }
+            bin_pic newPic(maxX, maxY);
+            for(int i = 0; i < maxX; i++){
+                for(int j = 0; j < maxY; j++){
+                    newPic.set(i, j, matrix[i][j] || pic.matrix[i][j]);
+                }
+            }
+            return newPic;
         }
-        return new_pic;
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
+        }
+        return *this;
     }
     // multiply two pictures
     bin_pic operator*(const bin_pic &pic){
-        bin_pic new_pic(maxX, maxY);
-        for(int i = 0; i < maxX; i++){
-            for(int j = 0; j < maxY; j++){
-                new_pic.set(i, j, matrix[i][j] && pic.matrix[i][j]);
+        try{
+            if(pic.maxX != maxX || pic.maxY != maxY){
+                throw MyException("Dimensions do not match");
             }
+            bin_pic newPic(maxX, maxY);
+            for(int i = 0; i < maxX; i++){
+                for(int j = 0; j < maxY; j++){
+                    newPic.set(i, j, matrix[i][j] && pic.matrix[i][j]);
+                }
+            }
+            return newPic;
         }
-        return new_pic;
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
+        }
+        return *this;
     }
     // subtract two pictures
     bin_pic operator-(const bin_pic &pic){
-        bin_pic new_pic(maxX, maxY);
-        for(int i = 0; i < maxX; i++){
-            for(int j = 0; j < maxY; j++){
-                new_pic.set(i, j, matrix[i][j] && !pic.matrix[i][j]);
+        try{
+            if(pic.maxX != maxX || pic.maxY != maxY){
+                throw MyException("Dimensions do not match");
             }
+            bin_pic newPic(maxX, maxY);
+            for(int i = 0; i < maxX; i++){
+                for(int j = 0; j < maxY; j++){
+                    newPic.set(i, j, matrix[i][j] && !pic.matrix[i][j]);
+                }
+            }
+            return newPic;
         }
-        return new_pic;
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
+        }
+        return *this;
     }
     // copy assignment
     bin_pic& operator=(const bin_pic &pic){
@@ -101,127 +170,230 @@ public:
             }
             maxX = pic.maxX;
             maxY = pic.maxY;
-            for(int i = 0; i < maxX; i++){
-                for(int j = 0; j < maxY; j++){
-                    matrix[i][j] = pic.matrix[i][j];
-                }
-            }
         }
         return *this;
     }
     // commutativity of addition
     friend bin_pic operator+(const bin_pic &pic1, const bin_pic &pic2){
-        bin_pic new_pic(pic1.maxX, pic1.maxY);
-        for(int i = 0; i < pic1.maxX; i++){
-            for(int j = 0; j < pic1.maxY; j++){
-                new_pic.set(i, j, pic1.matrix[i][j] || pic2.matrix[i][j]);
+        try{
+            if(pic1.maxX != pic2.maxX || pic1.maxY != pic2.maxY){
+                throw MyException("Dimensions do not match");
             }
+            bin_pic newPic(pic1.maxX, pic1.maxY);
+            for(int i = 0; i < pic1.maxX; i++){
+                for(int j = 0; j < pic1.maxY; j++){
+                    newPic.set(i, j, pic1.matrix[i][j] || pic2.matrix[i][j]);
+                }
+            }
+            return newPic;
         }
-        return new_pic;
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
+        }
+        return pic1;
     }
     // commutativity of multiplication
     friend bin_pic operator*(const bin_pic &pic1, const bin_pic &pic2){
-        bin_pic new_pic(pic1.maxX, pic1.maxY);
-        for(int i = 0; i < pic1.maxX; i++){
-            for(int j = 0; j < pic1.maxY; j++){
-                new_pic.set(i, j, pic1.matrix[i][j] && pic2.matrix[i][j]);
+        try{
+            if(pic1.maxX != pic2.maxX || pic1.maxY != pic2.maxY){
+                throw MyException("Dimensions do not match");
             }
+            bin_pic newPic(pic1.maxX, pic1.maxY);
+            for(int i = 0; i < pic1.maxX; i++){
+                for(int j = 0; j < pic1.maxY; j++){
+                    newPic.set(i, j, pic1.matrix[i][j] && pic2.matrix[i][j]);
+                }
+            }
+            return newPic;
         }
-        return new_pic;
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
+        }
+        return pic1;
     }
     // fill ratio
     double fill_ratio(){
-        int count = 0;
-        for(int i = 0; i < maxX; i++){
-            for(int j = 0; j < maxY; j++){
-                if(matrix[i][j]){
-                    count++;
+        try{
+            if(maxX == 0 || maxY == 0){
+                throw MyException("Dimensions are 0");
+            }
+            int count = 0;
+            for(int i = 0; i < maxX; i++){
+                for(int j = 0; j < maxY; j++){
+                    if(matrix[i][j]){
+                        count++;
+                    }
                 }
             }
+            return (double)count / (maxX * maxY);
         }
-        return (double)count / (maxX * maxY);
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
+        }
+        return 0;
     }
     // overloaded output operator
     friend std::ostream& operator<<(std::ostream &out, const bin_pic &pic){
-        for(int i = 0; i < pic.maxX; i++){
-            for(int j = 0; j < pic.maxY; j++){
-                out << pic.matrix[i][j];
+        try{
+            if(pic.maxX == 0 || pic.maxY == 0){
+                throw MyException("Dimensions are 0");
             }
-            out << std::endl;
+            for(int i = 0; i < pic.maxX; i++){
+                for(int j = 0; j < pic.maxY; j++){
+                    out << pic.matrix[i][j];
+                }
+                out << std::endl;
+            }
+            return out;
+        }
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
         }
         return out;
     }
     // overloaded input operator
     friend std::istream& operator>>(std::istream &in, bin_pic &pic){
-        for(int i = 0; i < pic.maxX; i++){
-            for(int j = 0; j < pic.maxY; j++){
-                in >> pic.matrix[i][j];
+        try{
+            if(pic.maxX == 0 || pic.maxY == 0){
+                throw MyException("Dimensions are 0");
             }
+            for(int i = 0; i < pic.maxX; i++){
+                for(int j = 0; j < pic.maxY; j++){
+                    in >> pic.matrix[i][j];
+                }
+            }
+            return in;
+        }
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
         }
         return in;
     }
     // overloaded equality operator
     friend bool operator==(const bin_pic &pic1, const bin_pic &pic2){
-        if(pic1.maxX != pic2.maxX || pic1.maxY != pic2.maxY){
-            return false;
-        }
-        for(int i = 0; i < pic1.maxX; i++){
-            for(int j = 0; j < pic1.maxY; j++){
-                if(pic1.matrix[i][j] != pic2.matrix[i][j]){
-                    return false;
+        try{
+            if(pic1.maxX != pic2.maxX || pic1.maxY != pic2.maxY){
+                throw MyException("Dimensions do not match");
+            }
+            for(int i = 0; i < pic1.maxX; i++){
+                for(int j = 0; j < pic1.maxY; j++){
+                    if(pic1.matrix[i][j] != pic2.matrix[i][j]){
+                        return false;
+                    }
                 }
             }
+            return true;
         }
-        return true;
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
+        }
+        return false;
     }
     // overloaded inequality operator
     friend bool operator!=(const bin_pic &pic1, const bin_pic &pic2){
-        if(pic1.maxX != pic2.maxX || pic1.maxY != pic2.maxY){
-            return true;
-        }
-        for(int i = 0; i < pic1.maxX; i++){
-            for(int j = 0; j < pic1.maxY; j++){
-                if(pic1.matrix[i][j] != pic2.matrix[i][j]){
-                    return true;
+        try{
+            if (pic1.maxX != pic2.maxX || pic1.maxY != pic2.maxY) {
+                throw MyException("Dimensions do not match");
+            }
+            for (int i = 0; i < pic1.maxX; i++) {
+                for (int j = 0; j < pic1.maxY; j++) {
+                    if (pic1.matrix[i][j] != pic2.matrix[i][j]) {
+                        return true;
+                    }
                 }
             }
+            return false;
+        }
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
         }
         return false;
     }
     // overloaded less than operator
     friend bool operator<(const bin_pic &pic1, const bin_pic &pic2){
-        if(pic1.maxX < pic2.maxX || pic1.maxY < pic2.maxY){
-            return true;
+        try{
+            if(pic1.maxX != pic2.maxX || pic1.maxY != pic2.maxY){
+                throw MyException("Dimensions do not match");
+            }
+            for(int i = 0; i < pic1.maxX; i++){
+                for(int j = 0; j < pic1.maxY; j++){
+                    if(pic1.matrix[i][j] < pic2.matrix[i][j]){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
         }
         return false;
     }
     // overloaded greater than operator
     friend bool operator>(const bin_pic &pic1, const bin_pic &pic2){
-        if(pic1.maxX > pic2.maxX || pic1.maxY > pic2.maxY){
-            return true;
+        try{
+            if(pic1.maxX != pic2.maxX || pic1.maxY != pic2.maxY){
+                throw MyException("Dimensions do not match");
+            }
+            for(int i = 0; i < pic1.maxX; i++){
+                for(int j = 0; j < pic1.maxY; j++){
+                    if(pic1.matrix[i][j] > pic2.matrix[i][j]){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
         }
         return false;
     }
     // overloaded less than or equal to operator
     friend bool operator<=(const bin_pic &pic1, const bin_pic &pic2){
-        if(pic1.maxX <= pic2.maxX || pic1.maxY <= pic2.maxY){
-            return true;
+        try{
+            if(pic1.maxX != pic2.maxX || pic1.maxY != pic2.maxY){
+                throw MyException("Dimensions do not match");
+            }
+            for(int i = 0; i < pic1.maxX; i++){
+                for(int j = 0; j < pic1.maxY; j++){
+                    if(pic1.matrix[i][j] <= pic2.matrix[i][j]){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
         }
         return false;
     }
     // overloaded greater than or equal to operator
     friend bool operator>=(const bin_pic &pic1, const bin_pic &pic2){
-        if(pic1.maxX >= pic2.maxX || pic1.maxY >= pic2.maxY){
-            return true;
+        try{
+            if (pic1.maxX != pic2.maxX || pic1.maxY != pic2.maxY) {
+                throw MyException("Dimensions do not match");
+            }
+            for (int i = 0; i < pic1.maxX; i++) {
+                for (int j = 0; j < pic1.maxY; j++) {
+                    if (pic1.matrix[i][j] >= pic2.matrix[i][j]) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        catch(MyException &e){
+            std::cout << e.what() << std::endl;
         }
         return false;
     }
-
 };
 
 int main() {
     bin_pic pic(5, 5);
-    pic.set(0, 0, true);
+    pic.set(9, 9, true);
     pic.set(1, 1, true);
     pic.set(2, 2, true);
     pic.set(3, 3, true);
