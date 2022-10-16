dbda14eb3fbcfaa533d1b15a1f8538ba5232b0e7
the80hz <dnl.vlgv@mail.ru>
2022-10-16 19:31:29 +0400

    Какой никакой main вроде есть

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ int main() {
     bin_pic pic1(7, 7);
     bin_pic::draw_circle(pic1, 3, 3, 2);
     std::cout << pic1 << std::endl;
-    std::cout << pic1.fill_ratio();
+    bin_pic pic2(7, 7);
+    bin_pic::draw_circle(pic2, 1, 1, 3);
+    std::cout << pic2 << std::endl;
+    bin_pic pic3 = pic1 + pic2;
+    std::cout << pic3 << std::endl;
     return 0;
 }
